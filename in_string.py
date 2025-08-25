7c24edbdb5122c303b5c1d57c077b53ef9368414
Lautaro Flores <lflores3@mail.austral.edu.ar>
2025-08-25 12:51:36 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,5 +1,5 @@
 def check_vowels():
-    # Código a implementar utilizando input.
+
     name_1 = input("Ingrese un nombre: ")
     print("")
 
@@ -19,8 +19,4 @@ def check_vowels():
     print("Contiene i:","i" in name_2.lower())
     print("Contiene o:","o" in name_2.lower())
     print("Contiene u:","u" in name_2.lower())
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
-
 
--- slice_advanced.py
+++ slice_advanced.py
@@ -4,7 +4,5 @@ def slice_advanced():
     txt_2 = input("Ingresar un texto: ")
     print(txt_2.lower()[4::2])
 
-    # Código a implementar utilizando input.
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py
+
